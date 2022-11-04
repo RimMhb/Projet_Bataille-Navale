51faaefed6f0b7d2c58d8c67e03c8168407c4678
Kenza-Mouloudi <116178499+Kenza-Mouloudi@users.noreply.github.com>
2022-11-04 16:08:46 +0100

    Update TP2.py

--- TP2.py
+++ TP2.py
@@ -20,3 +20,25 @@ class Vessel:
         if self.max_hits == 0:
             raise DestroyedError
         self.weapon.fire_at(x,y,z)
+
+        class Battlefield:
+    def __init__(self):
+        self.vessels = []
+
+    def add_vessel(self,vessel):
+        coor = vessel.get_coordinates()
+        max_hits = vessel.max_hits
+        for existing_vessel in self.vessels:
+            if existing_vessel.get_coordinates() == coor:
+                return False
+            max_hits += existing_vessel.max_hits
+        if max_hits > 22:
+            return False
+        self.vessels.append(vessel)
+    
+    def receive(self,x,y,z):
+        coor = (x,y,z)
+        for existing_vessel in self.vessels:
+            if existing_vessel.get_coordinates() == coor:
+                return True
+        return False
