e82a61f653dfd135da300ea079f7fa1dd5b67d73
InasLaajraoui <115099473+InasLaajraoui@users.noreply.github.com>
2022-11-04 13:45:20 +0100

    Update TP1.py

--- TP1.py
+++ TP1.py
@@ -16,5 +16,36 @@ class Weapon:
         self.ammunations -= 1
         if sqrt(x**2 + y**2) > self.range:
             raise OutOfRangeError
+            
+            
+"""Les 3 armes:"""
+
+"""Lance missile anti-surface"""
+class Arme_LMS(Weapon):
+    def __init__(self) -> None:
+        super().__init__(40, 30)
+    def fire_at(self,x, y, z):
+        super().fire_at(x,y,z)
+        if z != 0:
+            raise OutOfRangeError
+
+"""Lance missile anti-air"""
+class Arme_LMA(Weapon):
+    def __init__(self) -> None:
+        super().__init__(50, 40)
+    def fire_at(self,x, y,z):
+        super().fire_at(x,y,z)
+        if z <= 0:
+            raise OutOfRangeError
+
+"""Lance torpilles"""
+class Arme_LT(Weapon):
+    def __init__(self) -> None:
+        super().__init__(15, 20)
+    def fire_at(self,x, y,z):
+        super().fire_at(x,y,z)
+        if z > 0:
+            raise OutOfRangeError
+
 
  
